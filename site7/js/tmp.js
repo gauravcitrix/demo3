2550a296cf670dcb742145279de589d6bd5fb5cc
gauravcitrix <65597593+gauravcitrix@users.noreply.github.com>
2020-06-11 00:29:37 +0530

    Add files via upload

---
+++ site7/js/tmp.js
@@ -0,0 +1,11 @@
+
+var a=document.getElementById("citrix_feedback");
+function myfunc()
+{
+ a.style.color="white";
+}
+var b=document.getElementById("citrix_feedback1");
+function myfunc1()
+{
+ b.style.color="white";
+}
